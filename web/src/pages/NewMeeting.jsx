77c87d6586def5567b9e8e09d54fc293ee6c959f
karimi <behrouzkarimi1993@gmail.com>
2025-01-03 01:55:11 +0000

    prepare code for css styling

--- web/src/pages/NewMeeting.jsx
+++ web/src/pages/NewMeeting.jsx
@@ -269,18 +269,72 @@ function NewMeeting() {
 									))}
 								</datalist>
 								<button onClick={addStation}>Add</button>
+
 								<div>
 									<ul>
-										{copyOfMeetingStations.map((stationObject, index) => {
-											return (
-												<li key={index}>
-													{stationObject.station.station_name}
-													<button onClick={(e) => deleteStation(e, index)}>
-														Delete
-													</button>
-												</li>
-											);
-										})}
+										{copyOfMeetingStations.map((stationObject, index) => (
+											<li key={index} className="meeting-list-item">
+												<span>{stationObject.station.station_name}</span>
+												<button
+													onClick={(e) => deleteStation(e, index)}
+													className="delete-button"
+													aria-label={`Delete ${stationObject.station.station_name}`}
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														width="20"
+														height="20"
+														viewBox="0 0 64 64"
+													>
+														<rect
+															x="16"
+															y="22"
+															width="32"
+															height="34"
+															fill="red"
+															rx="4"
+														/>
+														<rect
+															x="12"
+															y="16"
+															width="40"
+															height="5"
+															fill="black"
+															rx="2"
+														/>
+														<rect
+															x="24"
+															y="11"
+															width="16"
+															height="6"
+															fill="inherit"
+															rx="2"
+														/>
+														<rect
+															x="22"
+															y="27"
+															width="4"
+															height="24"
+															fill="white"
+														/>
+														<rect
+															x="30"
+															y="27"
+															width="4"
+															height="24"
+															fill="white"
+														/>
+														<rect
+															x="38"
+															y="27"
+															width="4"
+															height="24"
+															fill="white"
+														/>
+													</svg>
+												</button>
+											</li>
+										))}
 									</ul>
 								</div>
 							</div>
