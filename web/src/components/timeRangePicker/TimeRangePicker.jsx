158439d435ad78523941ec0604d2c97df90e41f1
fikretellek <fikretellek@icloud.com>
2025-01-23 05:10:51 +0000

    time picker component invalid input check and message implemented, styling updated

--- web/src/components/timeRangePicker/TimeRangePicker.jsx
+++ web/src/components/timeRangePicker/TimeRangePicker.jsx
@@ -1,47 +1,158 @@
 import PropTypes from "prop-types";
+import { useCallback, useEffect, useState } from "react";
+
+import { isValidInterval } from "../../utils/validationUtils/validationUtils";
+
+import "./TimeRangePicker.css";
 
 const TimeRangePicker = ({
 	earliestStartTime,
 	latestStartTime,
 	handleMeetingChange,
 }) => {
+	const [earliest, setEarliest] = useState(earliestStartTime || "");
+	const [latest, setLatest] = useState(latestStartTime || "");
+	const [invalid, setInvalid] = useState({
+		status: false,
+		place: "",
+		message: "",
+	});
+	const memoizedHandleMeetingChange = useCallback(handleMeetingChange, [
+		handleMeetingChange,
+	]);
+
+	useEffect(() => {
+		if (latest) {
+			if (isValidInterval(earliest, latest)) {
+				memoizedHandleMeetingChange("earliestStartTime", earliest);
+				memoizedHandleMeetingChange("latestStartTime", latest);
+				setInvalid(false);
+			} else if (earliest) {
+				memoizedHandleMeetingChange("earliestStartTime", "");
+				setInvalid({
+					status: true,
+					place: "earliest",
+					message: "Earliest time cannot be after latest time",
+				});
+			} else {
+				setInvalid(false);
+			}
+		} else {
+			setInvalid(false);
+			memoizedHandleMeetingChange("earliestStartTime", earliest);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [earliest]);
+
+	useEffect(() => {
+		if (earliest) {
+			if (isValidInterval(earliest, latest)) {
+				memoizedHandleMeetingChange("earliestStartTime", earliest);
+				memoizedHandleMeetingChange("latestStartTime", latest);
+				setInvalid(false);
+			} else if (latest) {
+				memoizedHandleMeetingChange("latestStartTime", "");
+				setInvalid({
+					status: true,
+					place: "latest",
+					message: "Latest time cannot be before earliest time",
+				});
+			} else {
+				setInvalid(false);
+			}
+		} else {
+			setInvalid(false);
+			memoizedHandleMeetingChange("latestStartTime", latest);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [latest]);
+
+	function handleTimePickerChange(pickerType, value) {
+		switch (pickerType) {
+			case "earliestStartTime":
+				setEarliest(value);
+				break;
+			case "latestStartTime":
+				setLatest(value);
+				break;
+		}
+	}
+
+	function handleInvalidInput(e) {
+		if (!earliest) {
+			setInvalid({
+				status: true,
+				place: "earliest",
+				message: "Please enter a valid time",
+			});
+		} else if (!latest) {
+			setInvalid({
+				status: true,
+				place: "latest",
+				message: "Please enter a valid time",
+			});
+		}
+		e.preventDefault();
+		e.target.setCustomValidity("");
+	}
+
 	return (
 		<>
-			<div className="time-group">
+			<div className="time-group" style={{ position: "relative" }}>
 				<div className="form-group">
 					<input
-						className="time-picker"
+						className={
+							"time-picker" + (invalid.place === "earliest" ? " invalid" : "")
+						}
 						type="time"
 						id="earliest-start-time"
 						name="earliestStartTime"
 						required
-						value={earliestStartTime}
+						max={latest}
+						value={earliest}
 						onChange={(e) =>
-							handleMeetingChange("earliestStartTime", e.target.value)
+							handleTimePickerChange("earliestStartTime", e.target.value)
 						}
 						aria-required="true"
+						onInvalid={handleInvalidInput}
 					/>
-					<label htmlFor="earliest-start-time">Earliest Start Time</label>
+					<label
+						htmlFor="earliest-start-time"
+						className={invalid.place === "earliest" ? "invalid" : ""}
+					>
+						Earliest Start Time
+					</label>
 				</div>
 
 				<p>to</p>
 
 				<div className="form-group">
 					<input
-						className="time-picker"
+						className={
+							"time-picker" + (invalid.place === "latest" ? " invalid" : "")
+						}
 						type="time"
 						id="latest-start-time"
 						name="latestStartTime"
 						required
-						value={latestStartTime}
-						min={earliestStartTime}
+						min={earliest}
+						value={latest}
 						onChange={(e) =>
-							handleMeetingChange("latestStartTime", e.target.value)
+							handleTimePickerChange("latestStartTime", e.target.value)
 						}
 						aria-required="true"
+						onInvalid={handleInvalidInput}
 					/>
-					<label htmlFor="latest-start-time">Latest Start Time</label>
+					<label
+						htmlFor="latest-start-time"
+						className={invalid.place === "latest" ? "invalid" : ""}
+					>
+						Latest Start Time
+					</label>
 				</div>
+				{invalid.status && (
+					<div className="error-message">{invalid.message}</div>
+				)}
 			</div>
 		</>
 	);
